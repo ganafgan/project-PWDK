e78ecf54f1eaf5fc6f8b7bcb66778c571837d674
afgantaufiq <ganafgan@gmail.com>
2020-06-16 00:14:23 +0700

    add UploadPhoto

--- tokobuku/src/pages/index.js
+++ tokobuku/src/pages/index.js
@@ -2,10 +2,12 @@ import GetStarted from './GetStarted';
 import Splash from './Splash';
 import Register from './Register';
 import Login from './Login';
+import UploadPhoto from './UploadPhoto'
 
 export {
     GetStarted,
     Splash,
     Register,
-    Login
+    Login,
+    UploadPhoto
 }
