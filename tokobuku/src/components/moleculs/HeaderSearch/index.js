63e2875e09de5574fa96796ba5a00210dbefe330
saitama <klikace123@gmail.com>
2020-06-21 15:20:49 +0700

    nambah

--- tokobuku/src/components/moleculs/HeaderSearch/index.js
+++ tokobuku/src/components/moleculs/HeaderSearch/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { colors } from '../../../utils'
 import { Input, Gap } from '../../atoms'
 import { IconFavoriet, IconCart } from '../../../assets'
@@ -7,66 +7,21 @@ import Axios from 'axios'
 import { API_URL } from '../../../supports/constants/urlApi'
 
 
-const HeaderSearch = ({title, width, onChangeText, value}, props) => {
-    const [data, setData] = useState(null)
+const HeaderSearch = ({title, width, onChangeText, value, onPressFavorite}) => {
+    
 
-    useEffect(()=>{getDataWishlist()},[])
-
-    const getDataWishlist = () => {
-        let id_user = 1 
-
-        Axios.get(API_URL + 'wishlist/' + id_user)
-        .then((res)=>{
-            if(!res.data.error){
-                setData(res.data.data)
-            }
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-
-    const renderWishlist = () => {
-        return data.map((val)=>{
-            return(               
-
-                <View style={{flexDirection:"row",flex:1,justifyContent:'center', alignItems:'center'}}>
-                    <IconFavoriet onPress={()=>props.navigation.navigate('Wishlist', {id : val.id})} />
-                    <Gap width={5} />
-                    <IconCart />
-                </View>
-                
-            )
-        })
-    }
-
-    if(data === null){
-        return(
-            <View>
-                <Text>Loading . . . </Text>
-            </View>
-        )
-    }
-
-    if(data.length === 0){
-        return(
-            <View>
-                <Text>Loading . . . </Text>
-            </View>
-        )
-    }
-
+    
     
     return (
         <View style={styles.container}>
             <TextInput style={styles.input(width)} placeholder={title} onChangeText={onChangeText} value={value} />
-            {/* <View style={{flexDirection:"row",flex:1,justifyContent:'center', alignItems:'center'}}>
-                <IconFavoriet />
+            <View style={{flexDirection:"row",flex:1,justifyContent:'center', alignItems:'center'}}>
+                <TouchableOpacity onPress={onPressFavorite} >
+                    <IconFavoriet />
+                </TouchableOpacity>
                 <Gap width={5} />
                 <IconCart />
-            </View> */}
-
-            {renderWishlist()}
+            </View>            
         </View>
     )
 }
