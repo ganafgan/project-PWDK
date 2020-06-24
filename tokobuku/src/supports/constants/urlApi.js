ce151c1dfd2eceaa7bb3e457e8c9c9c65e820a91
saitama <klikace123@gmail.com>
2020-06-25 00:07:32 +0700

    EditLanding

--- tokobuku/src/supports/constants/urlApi.js
+++ tokobuku/src/supports/constants/urlApi.js
@@ -1,6 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 // export const API_URL = 'http://192.168.11.115:4000/'
+// export const API_URL = 'http://192.168.11.121:4000/'
 // export const API_URL = 'http://192.168.11.121:4000/'
 
 
 
-export const API_URL = 'http://192.168.11.107:4000/'
+export const API_URL = 'http://192.168.1.4:4000/'
